8e0febcfdd08f30fefaa4696247e25fb1aaf3a69
ArthurXu17 <46683531+ArthurXu17@users.noreply.github.com>
2022-04-28 23:42:30 -0400

    Update wordlist.h
    Removed small lists used for testing

--- wordlist.h
+++ wordlist.h
@@ -1,8 +1,4 @@
 // this module provides wordle_word_list_len secret words
-extern const int test_word_list_len;
-extern char *test_word_list[];
-extern const int small_word_list_len;
-extern char *small_word_list[];
 extern const int wordle_word_list_len; // 2309
 extern char *wordle_word_list[];
 
